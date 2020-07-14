ab0847d33eb4f312e1573215c74f5903b6e31816
Alexander Sierra <alexandercsierra@gmail.com>
2020-07-14 13:58:31 -0400

    edited config

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export default {
   oidc: {
     clientId: CLIENT_ID,
     issuer: ISSUER,
-    redirectUri: 'http://localhost:3000/implicit/callback',
+    redirectUri: 'https://extrapickles.vercel.app/implicit/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
